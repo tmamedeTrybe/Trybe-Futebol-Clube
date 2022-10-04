40400cb86d01b17e844faaf40fb11a4d99a9356d
Thiago Mamede <tmamede2@gmail.com>
2022-10-04 18:55:25 -0300

    FIX - Corrigindo validação do email

--- app/backend/src/services/userService.ts
+++ app/backend/src/services/userService.ts
@@ -9,13 +9,11 @@ class UserService {
   constructor(private UserModel: typeof User) {}
 
   public async login({ email, password }:IUserLogin) {
-    const userValid = await this.UserModel.findOne({ where: { email } });
-
-    if (!userValid) return { code: 401, erro: 'Incorrect email or password' };
-
     const validLogin = validateLogin({ email, password });
-
     if (validLogin.erro) return { code: validLogin.code, erro: validLogin.erro };
+
+    const userValid = await this.UserModel.findOne({ where: { email } });
+    if (!userValid) return { code: 401, erro: 'Incorrect email or password' };
 
     const checkPassword = compareSync(password, userValid.password);
 
